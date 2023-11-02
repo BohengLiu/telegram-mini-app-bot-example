932bbc06d0841ecad1972d64db0cdbfc8719662e
Robot.l <lbh9311@sina.com>
2023-11-02 11:24:51 +0800

    feat: red pocket

---
+++ src/app/redpocket/page.tsx
@@ -0,0 +1,9 @@
+
+export default function RedPocket() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-4">
+      <button>发送红包</button>
+      <button>红包历史</button>
+    </main>
+  );
+}
---
+++ src/app/swap/page.tsx
@@ -0,0 +1,9 @@
+import Web3App from "@/components/Web3Modal";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-4">
+      <Web3App />
+    </main>
+  );
+}
