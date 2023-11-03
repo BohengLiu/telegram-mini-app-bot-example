f1f1b6ca342731c222ad58c17698f5f360613af8
Robot.l <lbh9311@sina.com>
2023-11-03 15:47:34 +0800

    feat: add

--- src/app/game/components/GameView.tsx
+++ src/app/game/components/GameView.tsx
@@ -1,15 +1,21 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function GameView() {
   const [initDataUnsafe, setInitDataUnsafe] = useState<any | null>(null);
   useEffect(() => {
     if ((window as any).Telegram.WebApp.initDataUnsafe) {
       console.log((window as any).Telegram.WebApp.initDataUnsafe);
-      setInitDataUnsafe((window as any).Telegram.WebApp.initDataUnsafe)
+      setInitDataUnsafe((window as any).Telegram.WebApp.initDataUnsafe);
     }
   }, []);
-  
-  return <div>{JSON.stringify(initDataUnsafe)}</div>
+
+  return (
+    <div className="max-w-[100vw]">
+      {initDataUnsafe && <p>{`Hi, ${initDataUnsafe.username}`}</p>}
+      {initDataUnsafe && <p>{`user id is ${initDataUnsafe.id}`}</p>}
+      <p className=" break-all">{JSON.stringify(initDataUnsafe)}</p>
+    </div>
+  );
 }
